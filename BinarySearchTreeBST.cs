ca7058263e9bfb395f468f8223fe8737cfc42f41
Abhishek Jadhav <59304331+avzard@users.noreply.github.com>
2022-06-24 11:37:26 +0530

    Add files via upload

---
+++ BinarySearchTreeBST.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinarySearchTree_BST_
+{
+    class MyBinaryNode<T> where T : IComparable<T>
+    {
+        T NodeData;
+        public MyBinaryNode<T> leftTree { get; set; }
+        public MyBinaryNode<T> rightTree { get; set; }
+
+        public MyBinaryNode(T NodeData)
+        {
+            this.NodeData = NodeData;
+            leftTree = null;
+            rightTree = null;
+        }
+        public void Insert(T item)
+        {
+            T currentNodeValue = NodeData;
+            if ((currentNodeValue.CompareTo(item)) > 0)
+            {
+                if (leftTree == null)
+                    leftTree = new MyBinaryNode<T>(item);
+                else
+                    leftTree.Insert(item);
+            }
+            else
+            {
+                if (rightTree == null)
+                    rightTree = new MyBinaryNode<T>(item);
+                else
+                    rightTree.Insert(item);
+            }
+        }
+        public void Display()
+        {
+            if (leftTree != null)
+            {
+                leftTree.Display();
+            }
+            Console.WriteLine(NodeData.ToString());
+            if (rightTree != null)
+            {
+                rightTree.Display();
+            }
+        }
+    }
+}
